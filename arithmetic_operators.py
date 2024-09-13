e526c99d325a773b109027807188cf0bc87e1243
Indra Pranata <2450047@student.ppkia.ac.id>
2024-09-13 13:55:34 +0800

    Learn how to use different arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+addition = 33 + 36
+substraction = 100 - 31
+multiplication = 23 * 3
+division = 345 / 5
+modulus = 209 % 3
+exponentiation = 8.3 ** 2
+floor_division = 139 // 2
+
+print(addition)
+print(substraction)
+print(multiplication)
+print(division)
+print(modulus)
+print(exponentiation)
+print(floor_division)
